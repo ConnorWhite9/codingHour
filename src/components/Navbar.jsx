03777f85f832382fe1c22f49e04def59014b37ea
Connor <connorlwhite@icloud.com>
2024-10-30 16:50:38 -0400

    Added buttons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
 import react from "react";
+import logo from '../assets/badlogo.png';
 
 function Navbar() {
     return (
-        <header>
-            <p className="text-white">The Coding Hour</p>
-        </header>
+        <>
+            <hr className="border-2 border-[#7d007b]" />
+            <header className="h-[10vh] ">
+                <div class="p-[1rem] flex flex-row items-center">
+                    <img className="ml-[1rem]" src={logo}/>
+                    <p className=" ml-[1rem]">The Coding Hour</p>
+                    <button className="ml-[20rem] text-black font-semibold py-2 px-6 rounded-full border border-black shadow-lg bg-[#ffe8fa] hover:bg-blue-600 transition duration-300 ">Home</button>
+                    <button className="ml-[2rem] text-black font-semibold py-2 px-6 rounded-full border border-black shadow-lg bg-[#ffe8fa] hover:bg-blue-600 transition duration-300 ">About</button>
+                    <button className="ml-[2rem] text-black font-semibold py-2 px-6 rounded-full border border-black shadow-lg bg-[#ffe8fa] hover:bg-blue-600 transition duration-300 text-nowrap ">Get Involved</button>
+                    <button className="ml-[2rem] text-black font-semibold py-2 px-6 rounded-full border border-black shadow-lg bg-[#ffe8fa] hover:bg-blue-600 transition duration-300 ">Blog</button>
+                
+                </div>
+        
+            </header>
+        </>
     );
 }
 
