ddd9434e36379ff8a9a5241d133f6f8a8a12b7f3
Connor <connorlwhite@icloud.com>
2024-11-01 21:57:04 -0400

    Added leadership title and container for about page

---
+++ src/components/Leadership.jsx
@@ -0,0 +1,20 @@
+import react from "react";
+
+function Leadership () {
+    return (
+        <>
+            <div className="w-[100%]">
+                <p className="text-[5rem] text-center ml-[auto] mr-[auto]">Leadership</p>
+                <div className="w-[95rem] ml-[auto] mr-[auto] bg-indigo-300 p-[5rem]">
+                    <div className="flex flex-row">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+}
+
+
+export default Leadership
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import react from "react";
 import AboutDescrip from '../components/AboutDescrip';
+import Leadership from '../components/Leadership';
+
 
 function About () {
     return (
         <>
-            
             <AboutDescrip />
+            <Leadership />
         </>
     );
 }
