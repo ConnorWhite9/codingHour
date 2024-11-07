42dca425951d3e22933678d67a02a0417b756481
Connor <connorlwhite@icloud.com>
2024-11-06 22:09:22 -0500

    Added leadership photos

--- src/components/Leadership.jsx
+++ src/components/Leadership.jsx
@@ -1,5 +1,12 @@
 import react from "react";
 import ProfileContainer from './ProfileContainer';
+import helenahu from '../assets/helenahu.png';
+import emilychow from '../assets/emilychow.png';
+import aanya from '../assets/aanyauprethi.png';
+import hansika from '../assets/hansikakantheti.png';
+import vishal from '../assets/vishalvarma.png';
+import shaun from  '../assets/shauniyer.png';
+
 
 function Leadership () {
     return (
