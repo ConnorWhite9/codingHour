58cae624a0b4a662798c40dc1b798eb772610cce
Connor <connorlwhite@icloud.com>
2024-11-11 11:30:47 -0500

    Fixed up styling error on getInvolved due to changes in Navbar

--- src/pages/GetInvolved.jsx
+++ src/pages/GetInvolved.jsx
@@ -4,7 +4,7 @@ import Enroll from "../components/Enroll";
 function GetInvolved() {
     return (
         <>
-            <p className="font-corben text-center text-[3rem] mt-[3rem]">The Coding Hour Program</p>
+            <p className="font-corben text-center text-[3rem] mt-[7rem]">The Coding Hour Program</p>
             <Enroll />
             <p className="text-center py-[5rem]">Please contact thecodinghour1@gmail.com for any questions/inquries</p>
         </>
