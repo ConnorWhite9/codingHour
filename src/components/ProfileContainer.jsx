e2fd814ee94ef3298f077ee6a947a7ae07ce6528
Connor <connorlwhite@icloud.com>
2024-11-16 20:19:16 -0500

    Fixed up profile container text a little might need some more work

--- src/components/ProfileContainer.jsx
+++ src/components/ProfileContainer.jsx
@@ -6,11 +6,10 @@ function ProfileContainer ({photo, name, position}) {
         <>
             <div className="flex flex-col relative ml-[2rem]">
                 <div className=" w-[18rem] h-[18rem]" style={{backgroundImage: `url(${photo})`, backgroundSize: 'cover'}}>
-
                 </div>
                 <div className="bg-white h-[6rem] w-[15rem] absolute top-[16rem] left-[3rem] pl-[1rem] pt-[0.5rem]" >
-                    <p className="font-bold text-[1.2rem]">{name}</p>
-                    <p>{position}</p>
+                    <p className="font-bold font-fahkwang text-[1.2rem]">{name}</p>
+                    <p className="font-avenirLight font-thin">{position}</p>
                 </div>
             </div>
 
